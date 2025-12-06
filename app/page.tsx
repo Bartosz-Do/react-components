4911580121650252d4f2e0eb39415ea8830da348
Bartek <xbartoszdobija@gmail.com>
2025-12-07 00:46:55 +0100

    modified text in home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ export default function Page() {
                     <h1>Welcome!</h1>
                     <p>This is my website written in Next.js and React.js!</p>
                     <p>
-                        You can find here many standalone and opensource components. All code is public on my&nbsp;
+                        You can find here many standalone and opensource components. Whole code is public on my&nbsp;
                         <a target="_blank" href="https://github.com/Bartosz-Do/react-components">GitHub repository.</a>
-                        &nbsp;Feel free to check it out!
+                        &nbsp;Feel free to test it yourself!
                     </p>
                 </div>
         </main>
